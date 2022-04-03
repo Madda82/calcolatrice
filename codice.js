a70c8d54d51f155fdc0cc3aeb4434282286dcd91
Madda82 <maddalenacuozzo@gmail.com>
2022-04-03 22:43:20 +0200

    Funzione mostra ok - pari dispari quasi

--- codice.js
+++ codice.js
@@ -1,4 +1,4 @@
-function somma(numero1, numero2)
+/*function somma(numero1, numero2)
  {
    let risultato = numero1+numero2;   
         return risultato;
@@ -6,3 +6,24 @@ function somma(numero1, numero2)
 
 let la_mia_somma = somma(5+2);
 console.log (la_mia_somma);
+*/
+function mostra(valore) {
+  const risultatoDOM = document.querySelector('#risultato');
+  risultatoDOM.value = valore;
+}
+
+
+function PariDispari(numero)
+{
+    if (numero %2 == 1)
+    {
+        return risul="dispari";
+    }
+    else
+    {
+        return risul="pari";
+    }
+}
+
+let risultato=PariDispari(21);
+console.log ("il numero è" + " " + risultato );
