c510316b1c8ad850a269a44bdeaa2f47a516e200
Madda82 <maddalenacuozzo@gmail.com>
2022-05-17 22:59:29 +0200

    Calcolatrice quasi funzionante

--- codice.js
+++ codice.js
@@ -1,29 +1,133 @@
-/*function somma(numero1, numero2)
- {
-   let risultato = numero1+numero2;   
-        return risultato;
+let operando1;
+let operando2;
+let operatore;
+let risultato;
+const NUMERO_MASSIMO_CIFRE = 10;
+const risultatoDOM = document.querySelector('#risultato');
+
+function reset() { // resetta le variabili
+    operando1 = null;
+    operando2 = null;
+    operatore = null;
+    risultato = null;
+    mostra(""); // pulisce lo schermo della calcolatrice
+}
+
+
+function input(bottone) {
+
+    if (bottone === 'C') {
+        reset();
+    } else if (bottone === '=') {
+        risultato= esegui_operazione(operando1, operando2, operatore);
+        mostra(risultato);
+    } 
+    else if (['*', '-', '+', '/', '%'].includes(bottone)) {
+        console.log(operatore);
+
+        // caso1: 
+        if (operatore == null){
+            operatore=bottone;
+            mostra(risultatoDOM.value+operatore);
+        }else
+          {
+            // sono nel caso operando > operatore > operando > operatore >  es. 1 + 2 +
+            if ((operando1 =! null) && (operando2 !=null)){
+                risultato= esegui_operazione(operando1, operando2, operatore);
+                mostra(risultato);
+                operando1= risultato;
+                operatore= bottone;
+            }
+
+          }
+       
+    } 
+    else { // primo e secondo operando
+        if (operatore == null){
+            operando1 = acquisci_operando(operando1, bottone);
+        } 
+        else{
+            operando2 = acquisci_operando(operando2, bottone);
+        }
+        mostra(risultatoDOM.value+bottone);
+    }
+
+    
+}  
+
+             
+
+
+/**
+ * 
+ * @param {string} op 
+ * @param {number} val 
+ * @returns {string}
+ */
+function acquisci_operando(op, val) {
+    if (op === null) {
+        op = '';
+    }
+
+    if (typeof op === 'string' && op.length > NUMERO_MASSIMO_CIFRE) {
+        return op;
+    }
+
+    const nuovo = op.toString() + val.toString();
+    return nuovo;
+}
+
+function somma(op1, op2) { // assunzione: i parametri in input sono numeri
+    const ris = op1 + op2;
+    return ris;
+}
+
+function sottrazione(op1, op2) { // assunzione: i parametri in input sono numeri
+    const ris = op1 - op2;
+    return ris;
+}
+
+function moltiplicazione(op1, op2) { // assunzione: i parametri in input sono numeri
+    const ris = op1 * op2;
+    return ris;
+}
+
+/*
+function divisione(op1, op2) { // assunzione: i parametri in input sono numeri
+    risultato = op1 / op2;
+    return risultato;
+}
+*/
+
+function mostra(valore) {
+    risultatoDOM.value = valore;
+}
+
+function acquisci_operatore(oper) {
+    return oper;
   }
 
-let la_mia_somma = somma(5+2);
-console.log (la_mia_somma);
-*/
-function mostra(valore) {
-  const risultatoDOM = document.querySelector('#risultato');
-  risultatoDOM.value = valore;
-}
 
+function esegui_operazione (oper1, oper2, oper){
+    oper1= parseFloat(oper1);
+    oper2= parseFloat(oper2);
 
-function PariDispari(numero)
-{
-    if (numero %2 == 1)
-    {
-        return risul="dispari";
+ let ris = 0;
+ switch (oper) {
+    case '+':
+      ris= somma(oper1,oper2);
+     break;
+    case '-':
+        ris= sottrazione(oper1,oper2);
+     break;
+    case '*':
+        ris= moltiplicazione(oper1,oper2);
+      break;
+    default: 
+      ris=0;
     }
-    else
-    {
-        return risul="pari";
-    }
+   return ris;
 }
+// -----
 
-let risultato=PariDispari(2);
-console.log ("il numero è" + " " + risultato );
+reset();
